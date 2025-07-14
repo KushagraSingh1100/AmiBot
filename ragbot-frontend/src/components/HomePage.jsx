a29d8b053fac153d1c28d9b73bf1e557a11bd575
Kushagra <kushagrasingh1100@gmail.com>
2025-07-14 20:46:47 +0530

    connected backend with the frontend

--- ragbot-frontend/src/components/HomePage.jsx
+++ ragbot-frontend/src/components/HomePage.jsx
@@ -2,8 +2,37 @@ import Navbar from "./Navbar";
 import "./HomePage.css";
 import Bot from "/bot.png";
 import User from "/user.png";
+import { useState } from "react";
 
 function HomePage() {
+  const [convo, setConvo] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("");
+
+  const sendReq = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/query", {
+        method: "POST",
+        body: JSON.stringify({ query }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const res = await response.json();
+
+      if (!response.ok) {
+        loading(false);
+        throw new Error(res?.detail || "Something went wrong");
+      }
+      setConvo((prevItems) => [...prevItems, query, res[0].answer]);
+      setLoading(false);
+      setQuery("");
+    } catch (error) {
+      console.error("❌ Fetch error:", error.message);
+    }
+  };
+
   return (
     <div>
       <Navbar />
@@ -29,50 +58,54 @@ function HomePage() {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col items-end">
-              <p className="text-neutral-300 text-end text-sm mr-14">User</p>
-              <div className="flex flex-row-reverse items-end w-max h-max gap-1">
-                <img className="rounded-full w-10" src={User} alt="Bot" />
-                <div className="max-w-160 w-max h-max bg-neutral-100 p-4 py-3 rounded-lg flex items-center justify-start">
-                  <p className="w-max font-normal text-gray-700">
-                    Hi can you help me with my assignment questions?
+            {convo.map((msg, indx) =>
+              indx % 2 === 0 ? (
+                <div key={indx} className="flex flex-col items-end">
+                  <p className="text-neutral-300 text-end text-sm mr-14">
+                    User
                   </p>
+                  <div className="flex flex-row-reverse items-end w-max h-max gap-1">
+                    <img className="rounded-full w-10" src={User} alt="User" />
+                    <div className="max-w-160 w-max h-max bg-neutral-100 p-4 py-3 rounded-lg flex items-center justify-start">
+                      <p className="w-max font-normal text-gray-700">{msg}</p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="flex flex-col">
-              <p className="text-neutral-300 text-sm ml-14">AmiBot</p>
-              <div className="flex flex-row items-end w-max h-max gap-1">
-                <img className="rounded-full w-10" src={Bot} alt="Bot" />
-                <div className="max-w-160 w-max h-max bg-neutral-100 p-4 py-3 rounded-lg flex items-center justify-start">
-                  <p className="w-max font-normal text-gray-700">
-                    Yes why not ask me any question!
-                  </p>
+              ) : (
+                <div key={indx} className="flex flex-col">
+                  <p className="text-neutral-300 text-sm ml-14">AmiBot</p>
+                  <div className="flex flex-row items-end w-max h-max gap-1">
+                    <img className="rounded-full w-10" src={Bot} alt="Bot" />
+                    <div className="max-w-160 w-max h-max bg-neutral-100 p-4 py-3 rounded-lg flex items-center justify-start">
+                      <p className="w-max font-normal text-gray-700">{msg}</p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="flex flex-col items-end">
-              <p className="text-neutral-300 text-end text-sm mr-14">User</p>
-              <div className="flex flex-row-reverse items-end w-max h-max gap-1">
-                <img className="rounded-full w-10" src={User} alt="Bot" />
-                <div className="max-w-160 w-max h-max bg-neutral-100 p-4 py-3 rounded-lg flex items-center justify-end">
-                  <p className="w-max font-normal text-gray-700">
-                    What is operating system?
-                  </p>
-                </div>
-              </div>
-            </div>
+              )
+            )}
           </div>
         </div>
         <div className="fixed bottom-6 w-[70%] h-max flex flex-row">
           <textarea
             className="relative scrollbar-hide bg-neutral-100 min-h-12 max-h-100 rounded-lg rounded-r-none w-full px-4 py-2 resize-none overflow-y-scroll border-none outline-none focus:ring-0"
             placeholder="Type your question here..."
+            value={query}
             rows={1}
+            disabled={loading}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
           />
           <div className="rounded-lg rounded-l-none h-12 bg-neutral-100 flex justify-center items-center p-3">
-            <button className="bg-blue-500 text-white rounded-xl px-5 h-8 cursor-pointer transition duration-200 ease-in-out hover:opacity-65">
-              Send
+            <button
+              onClick={() => {
+                sendReq();
+                setLoading(true);
+              }}
+              disabled={loading}
+              className="bg-blue-500 text-white rounded-xl px-5 h-8 cursor-pointer transition duration-200 ease-in-out hover:opacity-65"
+            >
+              {loading ? "Sending" : "Send"}
             </button>
           </div>
         </div>
